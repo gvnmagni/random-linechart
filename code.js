afbe93fe9c1ff71d832c878bd0e2f85592ab6abe
Giovanni Magni <giovanni.magni@elastic.co>
2022-03-01 16:59:33 +0100

    Add files via upload

--- code.js
+++ code.js
@@ -1,4 +1,6 @@
-figma.showUI(__html__,  { width: 300, height: 520, title: "Random Linechart" });
+figma.showUI(__html__,  { width: 300, height: 580, title: "Random Linechart" });
+
+figma.loadFontAsync({ family: "Inter", style: "Regular" });
 
 figma.ui.onmessage = msg => {
   
@@ -11,6 +13,16 @@ figma.ui.onmessage = msg => {
     const circleSize = msg.dotsSize;
     let randomDomain = msg.height;
     const identifier = Math.floor(Math.random()*1000);
+
+    let leftLabelsMargin = 0;
+    let bottomLabelsMargin = 0;
+    if ((msg.grid === "yes") && (msg.labels === "yes")){
+        leftLabelsMargin = 50;
+        bottomLabelsMargin = 30;
+    }   
+    
+
+    const labels_color = {r:106/255, g:113/255, b:125/255};
 
     const colors = [{r:96, g:146, b:192},
         {r:84, g:179, b:153},
@@ -32,22 +44,24 @@ figma.ui.onmessage = msg => {
     // ---------------------------------
     // Define random domain according to variability
     let offset = 0;
-    if (msg.variability === "high")
-        randomDomain = msg.height;
+    if (msg.variability === "high"){
+        randomDomain = msg.height-bottomLabelsMargin;
+        offset = 0;
+    }        
     else if (msg.variability === "medium") {
-        randomDomain = msg.height/2;
-        offset = msg.height/(2*2);
+        randomDomain = (msg.height-bottomLabelsMargin)/2;
+        offset = ((msg.height-bottomLabelsMargin)/4);
     }
     else if (msg.variability === "low") {
-        randomDomain = msg.height/3;
-        offset = msg.height/(3*2);
+        randomDomain = (msg.height-bottomLabelsMargin)/3;
+        offset = ((msg.height-bottomLabelsMargin)/3);
     }
 
     // ---------------------------------
         // Drag grid if required
 
         const grid_strokes = [{ type: 'SOLID', color: {r: 0.8, g: 0.8, b: 0.8}}];
-                
+        const today = new Date();                
         if (msg.grid == "yes")
         {
             for (var i=0; i<msg.pointsCount; i++){
@@ -55,24 +69,55 @@ figma.ui.onmessage = msg => {
             grid_vertical_line.name = "grid_vertical_line_"+identifier;
             grid_vertical_line.vectorPaths = [{
                 windingRule: "NONE",
-                data: "M "+ (i*(msg.width/(msg.pointsCount-1))) +" 0 L "+ (i*(msg.width/(msg.pointsCount-1))) +" " + msg.height,
+                data: "M "+ (leftLabelsMargin+(i*((msg.width-leftLabelsMargin)/(msg.pointsCount-1)))) +" 0 L "+ (leftLabelsMargin+(i*((msg.width-leftLabelsMargin)/(msg.pointsCount-1)))) +" " + (msg.height-bottomLabelsMargin),
             }]    
         
             grid_vertical_line.strokes = grid_strokes;
             chart_frame.appendChild(grid_vertical_line);
             nodes.push(grid_vertical_line)
+
+            if (msg.labels == "yes"){
+
+                today.setDate(today.getDate()+1);
+
+                const bottom_label = figma.createText();
+                bottom_label.fontName = { family: "Inter", style: "Regular" };
+                bottom_label.characters = today.getMonth()+"/"+today.getDate();
+                bottom_label.textAlignHorizontal = "CENTER";
+                bottom_label.resize(40,16);
+                bottom_label.fontSize = 10;
+                bottom_label.setRangeFills(0,bottom_label.characters.length, [{ type: 'SOLID', color: labels_color}]);
+                bottom_label.x = (leftLabelsMargin+(i*((msg.width-leftLabelsMargin)/(msg.pointsCount-1))))-20;
+                bottom_label.y = (msg.height-bottomLabelsMargin+5);
+                bottom_label.name = "bottom_label_"+identifier;
+            }
         }
         for (var i=0; i<5; i++){
             const grid_horizontal_line = figma.createVector();
             grid_horizontal_line.name = "grid_horizontal_line_"+identifier;
             grid_horizontal_line.vectorPaths = [{
                 windingRule: "NONE",
-                data: "M 0 "+ (msg.height/4*i) + " L "+ msg.width +" "+(msg.height/4*i),
+                data: "M " + leftLabelsMargin + " "+ (((msg.height-bottomLabelsMargin)/4*i)) + " L "+ msg.width +" "+(((msg.height-bottomLabelsMargin)/4*i)),
             }]    
             
             grid_horizontal_line.strokes = grid_strokes;
             chart_frame.appendChild(grid_horizontal_line);
             nodes.push(grid_horizontal_line)
+
+            if (msg.labels == "yes"){
+                const left_label = figma.createText();
+                left_label.fontName = { family: "Inter", style: "Regular" };
+                left_label.characters = ((4-i)*100)+"K";
+                left_label.textAlignHorizontal = "RIGHT";
+                left_label.resize(30,16);
+                left_label.fontSize = 10;
+                left_label.setRangeFills(0,left_label.characters.length, [{ type: 'SOLID', color: labels_color}]);
+                left_label.x = 10;
+                left_label.y = (((msg.height-bottomLabelsMargin)/4*i))-8;
+                left_label.name = "left_label_"+identifier;
+
+
+            }
         }  
         
             const vertical_grid = figma.currentPage.findAll(n => n.name === "grid_vertical_line_"+identifier);
@@ -81,6 +126,16 @@ figma.ui.onmessage = msg => {
             vertical_grid_group.name = "Vertical Grid Lines";
             const horizontal_grid_group = figma.group(horizontal_grid, chart_frame);
             horizontal_grid_group.name = "Horizontal Grid Lines";
+
+            if (msg.labels == "yes"){
+                const left_labels = figma.currentPage.findAll(n => n.name === "left_label_"+identifier);
+                const bottom_labels = figma.currentPage.findAll(n => n.name === "bottom_label_"+identifier);
+                const left_labels_group = figma.group(left_labels, chart_frame);
+                left_labels_group.name = "Left Labels";
+                const bottom_labels_group = figma.group(bottom_labels, chart_frame);
+                bottom_labels_group.name = "Bottom Labels";
+            }
+
         
         }
 
@@ -91,6 +146,8 @@ figma.ui.onmessage = msg => {
         const color = {r:colors[k].r/255, g:colors[k].g/255, b:colors[k].b/255};
         const path = figma.createVector();
         path.name = "Line_"+k;
+        path.strokeCap = "ROUND";
+        path.strokeJoin = "ROUND";
         let d = "";
 
         // ---------------------------------
@@ -98,8 +155,8 @@ figma.ui.onmessage = msg => {
         let points = [];
         for (var i=0; i<msg.pointsCount; i++)
         {
-            y = Math.floor(Math.random()*randomDomain)+offset;
-            x = i*(msg.width/(msg.pointsCount-1));
+            y = (Math.floor(Math.random()*randomDomain)+offset)
+            x = leftLabelsMargin + (i*((msg.width-leftLabelsMargin)/(msg.pointsCount-1)));
             points.push({x : x, y : y});
         }
 
@@ -111,7 +168,7 @@ figma.ui.onmessage = msg => {
             for (var i=0; i<msg.pointsCount; i++)
             {
             if (i == 0)
-                d = d + "M 0 " + points[i].y;    
+                d = d + "M "+ leftLabelsMargin +" " + points[i].y;    
             else
                 d = d + " L " + points[i].x + " " + points[i].y;
             }
@@ -119,7 +176,7 @@ figma.ui.onmessage = msg => {
             for (var i=0; i<(msg.pointsCount-1); i++)
             {
             if (i == 0)
-                d = d + "M 0 " + points[i].y + " C " + (points[i].x+bezier_handler_x) + " " + points[i].y + " " + (points[i+1].x-bezier_handler_x) + " " + points[i+1].y + " " +points[i+1].x + " " + points[i+1].y;    
+                d = d + "M "+ leftLabelsMargin +" " + points[i].y + " C " + (points[i].x+bezier_handler_x) + " " + points[i].y + " " + (points[i+1].x-bezier_handler_x) + " " + points[i+1].y + " " +points[i+1].x + " " + points[i+1].y;    
             else
                 d = d + " C " + (points[i].x+bezier_handler_x) + " " + points[i].y + " " + (points[i+1].x-bezier_handler_x) + " " + points[i+1].y + " " +points[i+1].x + " " + points[i+1].y;
             }
@@ -153,12 +210,12 @@ figma.ui.onmessage = msg => {
                 circle.x = points[i].x - circleSize/2;
                 circle.y = points[i].y - circleSize/2; 
                 circle.fills = [{type: 'SOLID', color: {r:1, g:1, b:1}}];
-                
+
                 circle.strokes = strokes;
                 circle.strokeWeight = 2;
                 circle.strokeAlign = 'OUTSIDE'
 
-                circle.resize(circleSize, circleSize)
+                circle.resize(circleSize, circleSize);
                 chart_frame.appendChild(circle);
                 nodes.push(circle)
             }
